8a82ded19e5fb3f3bb72682115342747b7af0629
Daniel Voogsgerd <daniel@voogsgerd.nl>
2025-03-22 13:14:16 +0100

    refactor(cc): Use thiserror for Errors

--- brane-cc/src/errors.rs
+++ brane-cc/src/errors.rs
@@ -12,69 +12,49 @@
 //!   Defines errors for the `brane-cc` crate.
 //
 
-use std::error::Error;
-use std::fmt::{Display, Formatter, Result as FResult};
 use std::path::PathBuf;
 
 
 /***** LIBRARY *****/
 /// Collects errors that relate to offline compilation.
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum CompileError {
     /// Failed to open the given input file.
-    InputOpenError { path: PathBuf, err: std::io::Error },
+    #[error("Failed to open input file '{}'", path.display())]
+    InputOpenError { path: PathBuf, source: std::io::Error },
     /// Failed to read from the input.
-    InputReadError { name: String, err: std::io::Error },
+    #[error("Failed to read from input '{name}'")]
+    InputReadError { name: String, source: std::io::Error },
     /// Failed to fetch the remote package index.
-    RemotePackageIndexError { endpoint: String, err: brane_tsk::api::Error },
+    #[error("Failed to fetch remote package index from '{endpoint}'")]
+    RemotePackageIndexError { endpoint: String, source: brane_tsk::api::Error },
     /// Failed to fetch the remote data index.
-    RemoteDataIndexError { endpoint: String, err: brane_tsk::api::Error },
+    #[error("Failed to fetch remote data index from '{endpoint}'")]
+    RemoteDataIndexError { endpoint: String, source: brane_tsk::api::Error },
     /// Failed to fetch the local package index.
-    LocalPackageIndexError { err: brane_tsk::local::Error },
+    #[error("Failed to fetch local package index")]
+    LocalPackageIndexError { source: brane_tsk::local::Error },
     /// Failed to fetch the local data index.
-    LocalDataIndexError { err: brane_tsk::local::Error },
+    #[error("Failed to fetch local data index")]
+    LocalDataIndexError { source: brane_tsk::local::Error },
     /// Failed to serialize workflow.
-    WorkflowSerializeError { err: serde_json::Error },
+    #[error("Failed to serialize the compiled workflow")]
+    WorkflowSerializeError { source: serde_json::Error },
     /// Failed to create the given output file.
-    OutputCreateError { path: PathBuf, err: std::io::Error },
+    #[error("Failed to create output file '{}'", path.display())]
+    OutputCreateError { path: PathBuf, source: std::io::Error },
     /// Failed to write to the given output file.
-    OutputWriteError { name: String, err: std::io::Error },
+    #[error("Failed to write to output '{name}'")]
+    OutputWriteError { name: String, source: std::io::Error },
 
     /// Compilation itself failed.
-    CompileError { errs: Vec<brane_ast::Error> },
+    #[error("Failed to compile given workflow (see output above)")]
+    CompileError { sources: Vec<brane_ast::Error> },
 }
-
-impl Display for CompileError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> FResult {
-        use self::CompileError::*;
-        match self {
-            InputOpenError { path, err } => write!(f, "Failed to open input file '{}': {}", path.display(), err),
-            InputReadError { name, err } => write!(f, "Failed to read from input '{name}': {err}"),
-            RemotePackageIndexError { endpoint, err } => write!(f, "Failed to fetch remote package index from '{endpoint}': {err}"),
-            RemoteDataIndexError { endpoint, err } => write!(f, "Failed to fetch remote data index from '{endpoint}': {err}"),
-            LocalPackageIndexError { err } => write!(f, "Failed to fetch local package index: {err}"),
-            LocalDataIndexError { err } => write!(f, "Failed to fetch local data index: {err}"),
-            WorkflowSerializeError { err } => write!(f, "Failed to serialize the compiled workflow: {err}"),
-            OutputCreateError { path, err } => write!(f, "Failed to create output file '{}': {}", path.display(), err),
-            OutputWriteError { name, err } => write!(f, "Failed to write to output '{name}': {err}"),
-
-            CompileError { .. } => write!(f, "Failed to compile given workflow (see output above)"),
-        }
-    }
-}
-
-impl Error for CompileError {}
 
 
 
 /// Defines errors that occur when attempting to parse an IndexLocationParseError.
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
+#[error("The impossible has happened; an IndexLocationParseError was raised, even though none exist")]
 pub struct IndexLocationParseError;
-
-impl Display for IndexLocationParseError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> FResult {
-        write!(f, "The impossible has happened; an IndexLocationParseError was raised, even though none exist")
-    }
-}
-
-impl Error for IndexLocationParseError {}
